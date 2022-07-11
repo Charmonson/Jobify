fb8980a7e0905c80b0ee83ec4cc90cba07c2350a
Charmonson <77593573+Charmonson@users.noreply.github.com>
2022-07-10 20:13:10 -0500

    styled components 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Landing from "./pages/Landing";
 function App() {
   return (
     <div>
-      <h1>Jobify</h1>
+      
       <Landing/>
     </div>
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,7 +4,34 @@ import main from '../assets/images/main.svg';
 
 const Landing = () => {
   return (
-    <h4>Landing Page</h4>
+   <main>
+    <nav>
+        <img src={logo} alt='jobify' className='logo' />
+    </nav>
+    <div className='container page'>
+        {/*info*/}
+        <div className='info'>
+            <h1>
+                job <span>tracking</span> app
+            </h1>
+            <p>Deep v dolore raclette, 
+                proident hot chicken post-ironic 
+                pug ex enamel pin sed tattooed 
+                seitan vaporware copper mug. 
+                Four loko green juice put a bird on 
+                it cornhole venmo plaid intelligentsia 
+                esse. Four loko nisi kickstarter food
+                truck yuccie. Glossier viral 
+                church-key eu.</p>
+                <button className="btn btn-hero">
+                    Login/Register
+                </button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img"/>
+
+    </div>
+
+   </main>
   )
 }
 
