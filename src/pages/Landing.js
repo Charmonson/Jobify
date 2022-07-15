5ef62bd82a8f7fa90d7b0abcd08da3886c97ba46
Charmonson <77593573+Charmonson@users.noreply.github.com>
2022-07-15 18:35:13 -0500

    landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from 'styled-components';
 
 const Landing = () => {
   return (
-   <main>
+   <Wrapper>
     <nav>
         <img src={logo} alt='jobify' className='logo' />
     </nav>
@@ -31,8 +32,47 @@ const Landing = () => {
 
     </div>
 
-   </main>
+   </Wrapper>
   )
 }
+
+const Wrapper = styled.main `
+    nav{
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items:center;
+    }
+    .page{
+        min-height: calc(100vh - var(--nav-height));
+        display:grid;
+        align-self: start;
+        align-items: center;
+        margin-top: -3rem;
+    } 
+    h1 {
+        font-weight: 700;
+        span{
+            color: var(--primary-500);
+        }
+    }
+    p{
+        color:var(--grey-600);
+    }
+    .main-img{
+        display:none;
+    }
+    @media (min-width:992px) {
+        .page{
+            grid-template-columns: 1fr 1fr;
+            column-gap: 3rem;
+        }
+        .main-img{
+            display:block;
+        }
+    }
+     `
 
 export default Landing;
