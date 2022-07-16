95a6fff75ea56ede03e40fe4a56d6e97e229f856
Charmonson <77593573+Charmonson@users.noreply.github.com>
2022-07-16 13:27:43 -0500

    logo

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
+import {Logo} from '../components';
+import main from '../assets/images/main-alternative.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
   return (
    <Wrapper>
     <nav>
-        <img src={logo} alt='jobify' className='logo' />
+    <Logo/>
     </nav>
     <div className='container page'>
         {/*info*/}
